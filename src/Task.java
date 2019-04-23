a230bb0764b86ba23fcbcb83e4a46e0348065509
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 10:24:02 +0500

    last2

--- src/Task.java
+++ src/Task.java
@@ -4,19 +4,21 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(stringBits("Heeololeo"));
+        System.out.println(last2("axxxaaxx"));
 
 
     }
 
-    static public String stringBits(String str) {
-        String result = "";
-        for (int i = 0; i < (str.length()); i = i + 2) {
-            result = result + str.substring(i, i + 1);
+    static public int last2(String str) {
+        int count=0;
+        for(int i=0;i<str.length()-2;i++){
+            if(str.substring(i,i+2).equals(str.substring(str.length()-2,str.length()))){
+                count++;
+            }
+        }return count;
 
-        }
-        return result;
     }
+
 
 
 }
