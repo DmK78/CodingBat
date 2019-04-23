1bab55e2dfa7d33145841f98f97a78a76a0d9bab
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 10:31:49 +0500

    arrayCount9

--- src/Task.java
+++ src/Task.java
@@ -4,21 +4,22 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(last2("axxxaaxx"));
+        int numbers[] = {1, 9, 9, 3, 9};
+        System.out.println(arrayCount9(numbers));
 
 
     }
 
-    static public int last2(String str) {
-        int count=0;
-        for(int i=0;i<str.length()-2;i++){
-            if(str.substring(i,i+2).equals(str.substring(str.length()-2,str.length()))){
+    static public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 9) {
                 count++;
             }
-        }return count;
+        }
+        return count;
 
     }
-
 
 
 }
