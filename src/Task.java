94b469634dde547e1e1531ac5590c377f340393e
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 15:34:26 +0500

    twoChar

--- src/Task.java
+++ src/Task.java
@@ -4,15 +4,13 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(twoChar("java", 3));
+        System.out.println(middleThree("dsdasdasdsadsad"));
 
 
     }
 
-    static public String twoChar(String str, int index) {
-        if(str.length()-(index)>=2 && index>0){
-            return str.substring(index,index+2);
-        }else return str.substring(0,2);
+    static public String middleThree(String str) {
+        return str.substring(str.length()/2-1,str.length()/2+2);
 
     }
 }
