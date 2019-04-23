0d10e3a61cd379baa89e4f161514e8c282aa7148
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 11:58:11 +0500

    stringYak

--- src/Task.java
+++ src/Task.java
@@ -4,21 +4,15 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(altPairs("CodingHorror"));
+        System.out.println(stringYak("yak123ya"));
 
 
     }
 
-    static public String altPairs(String str) {
-        String result = "";
-        for (int i = 0; i < str.length(); i = i + 4) {
-            if (i <= str.length() - 1) {
-                result = result + str.substring(i, i + 1);
-            }
-            if (i <= str.length() - 2) {
-                result = result + str.substring(i + 1, i + 2);
-            }
-        }
-        return result;
+    static public String stringYak(String str) {
+       while (str.indexOf("yak")!=-1){
+           str=str.substring(0,str.indexOf("yak"))+str.substring(str.indexOf("yak")+3,str.length());
+       }
+       return str;
     }
 }
