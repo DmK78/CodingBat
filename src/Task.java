a4cb8fa7c83ef6dd8760cae3e15e9b1e963c76d5
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 12:42:20 +0500

    has271

--- src/Task.java
+++ src/Task.java
@@ -4,19 +4,21 @@ public class Task {
     public static void main(String[] args) {
 
 
-        int numbers[] = {6, 6, 2, 6,6,5,4,1,1,1};
+        int numbers[] = {2, 7, 4};
         System.out.println(noTriples(numbers));
 
 
     }
 
     static public boolean noTriples(int[] nums) {
-     for (int i=0;i<nums.length-2;i++){
-         int num1=nums[i];
-         if (nums[i+1]==num1 && nums[i+2]==num1){
-             return false;
-         }
-     }
-     return true;
+        for (int i = 0; i < nums.length - 2; i++) {
+            int num = nums[i];
+            if (nums[i + 1] == num + 5 && Math.abs(nums[i + 2]-(num-1)) <= 2) {
+                return true;
+            }
+
+
+        }
+        return false;
     }
 }
