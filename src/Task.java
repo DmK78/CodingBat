11def7e925d0ba3f154f73495cb77716468ddb02
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-23 10:50:49 +0500

    array123

--- src/Task.java
+++ src/Task.java
@@ -4,24 +4,20 @@ public class Task {
     public static void main(String[] args) {
 
 
-        int numbers[] = {1, 2, 4, 6, 4, 5};
-        System.out.println(arrayFront9(numbers));
+        int numbers[] = {1, 2};
+        System.out.println(array123(numbers));
 
 
     }
 
-    static public boolean arrayFront9(int[] nums) {
+    static public boolean array123(int[] nums) {
 
-        if (nums.length <= 3) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 9) {
-                    return true;
-                }
-            }
-        } else {
-            for (int i = 0; i <= 3; i++) {
-                if (nums[i] == 9) {
-                    return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (i < nums.length-2 && nums[i + 1] == 2) {
+                    if (nums[i + 2] == 3) {
+                        return true;
+                    }
                 }
             }
         }
