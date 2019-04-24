e074a8566be9cd1988aaa2db35c573a95cbd3d5e
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-24 12:29:34 +0500

    midThree

--- src/Task.java
+++ src/Task.java
@@ -9,13 +9,12 @@ public class Task {
 
     }
 
-    static public int[] swapEnds(int[] nums) {
-        int arr[] = new int[nums.length];
-        int tmp = 0;
-        tmp = nums[0];
-        nums[0] = nums[nums.length - 1];
-        nums[nums.length - 1] = tmp;
-        return nums;
+    static public int[] midThree(int[] nums) {
+        int arr[] = new int[3];
+        arr[0] = nums[nums.length / 2 - 1];
+        arr[1] = nums[nums.length / 2];
+        arr[2] = nums[nums.length / 2 + 1];
+        return arr;
 
     }
 }
