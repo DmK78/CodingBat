976d3bf4629d2881a6e31d420398a8eb8765cf25
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-24 11:35:36 +0500

    reverse3

--- src/Task.java
+++ src/Task.java
@@ -3,18 +3,18 @@ public class Task {
 
     public static void main(String[] args) {
 
-        int number[] = {1,2,3};
-        System.out.println(rotateLeft3(number).toString();
+        int number[] = {1, 2, 3};
+        System.out.println(reverse3(number));
 
 
     }
 
-    static public int[] rotateLeft3(int[] nums) {
-        int temp = nums[0];
-        for (int i = 0; i < nums.length - 1; i++) {
-            nums[i] = nums[i + 1];
+    static public int[] reverse3(int[] nums) {
+        int reverse[] = new int[nums.length];
+        for (int i = 0; i < nums.length ; i++) {
+            reverse[reverse.length - i-1] = nums[i];
         }
-        nums[nums.length - 1] = temp;
-        return nums;
+
+        return reverse;
     }
 }
