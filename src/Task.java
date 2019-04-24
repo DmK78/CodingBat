6c5c047e2d667ddba5de0f173c6c275d2520517f
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-24 21:53:02 +0500

    roundSum

--- src/Task.java
+++ src/Task.java
@@ -3,34 +3,40 @@ public class Task {
 
     public static void main(String[] args) {
 
-        int number[] = {1, 7};
-        int number1[] = new int[0];
-        int[] arr = (front11(number, number1));
-        for (int i : arr) {
-            System.out.println(i);
+
+        System.out.println(roundSum(4, 6, 5));
         }
 
+    static public int roundSum(int a, int b, int c) {
+        return round10(a) + round10(b) + round10(c);
 
     }
 
-    static public int noTeenSum(int a, int b, int c) {
-        return fixTeen(a) + fixTeen(b) + fixTeen(c);
-
-
-    }
-
-    static public int fixTeen(int n) {
-        switch (n) {
-            case 13:
-            case 14:
-            case 17:
-            case 18:
-            case 19: {
-                return 0;
-            }
-
+    static public int round10(int num) {
+        if (num<=10){
+            if (num>4){
+                return 10;
+            }else return 0;
         }
-        return n;
+        String resultString;
+        int resultInt;
+        int decimal;
 
+        String numToStr = String.valueOf(num);
+        String lastSymbolStr=numToStr.substring(numToStr.length()-1);
+
+        int roundInt = Integer.parseInt(lastSymbolStr);
+        if(roundInt<5){
+            resultString=numToStr.substring(0,numToStr.length()-1)+"0";
+            resultInt=Integer.parseInt(resultString);
+            return resultInt;
+        }
+        decimal=Integer.parseInt(numToStr.substring(numToStr.length()-2,numToStr.length()-1));
+        decimal++;
+        String decimalStr=String.valueOf(decimal);
+
+        resultString=numToStr.substring(0,numToStr.length()-2)+decimalStr+"0";
+        resultInt=Integer.parseInt(resultString);
+        return resultInt;
     }
 }
