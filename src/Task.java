267718987873c4c43adb4cf40fca2edf0293fd8b
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-24 11:51:28 +0500

    sum2

--- src/Task.java
+++ src/Task.java
@@ -9,15 +9,10 @@ public class Task {
 
     }
 
-    static public int[] maxEnd3(int[] nums) {
-        int max;
-        if (nums[0] > nums[nums.length - 1]) {
-            max = nums[0];
-        } else max = nums[nums.length - 1];
-
-        for (int i=0;i<nums.length;i++){
-            nums[i]=max;
-        }
-        return nums;
+    static public int sum2(int[] nums) {
+        if (nums.length==0)return 0;
+        if (nums.length>1){
+            return nums[0]+nums[1];
+        }else return nums[0]+nums[0];
     }
 }
