1a61f03a6f4b8109326625b492def2bf06667473
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-24 11:14:05 +0500

    firstLast6

--- src/Task.java
+++ src/Task.java
@@ -3,33 +3,17 @@ public class Task {
 
     public static void main(String[] args) {
 
-
-        System.out.println(withoutX2("xxHi"));
+        int number[] = {1, 2, 3, 4, 5, 6};
+        System.out.println(firstLast6(number));
 
 
     }
 
-    static public String withoutX2(String str) {
-        String x1="";
-        String x2="";
-        if (str.length()<2){
-            if (str.equals("x")){
-                return "";
-            }
-            return str;
+    static public boolean firstLast6(int[] nums) {
+        if (nums[0] == 6 || nums[nums.length-1] == 6) {
+            return true;
         }
-        if (str.charAt(0)=='x'){
-            x1="";
-        }else x1=str.substring(0,1);
-
-        if (str.charAt(1)=='x'){
-            x2="";
-        }else x2=str.substring(1,2);
-        return x1+x2+str.substring(2,str.length());
-
-
-
-
+        return false;
 
     }
 }
