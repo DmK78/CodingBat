cb4eaf44ce8e5159737de7d92b1810446ca37b73
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 21:14:04 +0500

    dateFashion

--- src/Task.java
+++ src/Task.java
@@ -10,13 +10,11 @@ public class Task {
     }
 
 
-    static public boolean cigarParty(int cigars, boolean isWeekend) {
-        if (isWeekend) {
-            if (cigars >= 40) return true;
-        } else {
-            if (cigars >= 40 && cigars <= 60) return true;
-        }
-        return false;
+    static public int dateFashion(int you, int date) {
+        if (date<3 || you<3) return 0;
+        if (you>7 || date>7) return 2;
+        return 1;
+
     }
 
 }
