16bc94f810359a6fa819acc88e87fb46a3c887e6
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 21:35:11 +0500

    alarmClock

--- src/Task.java
+++ src/Task.java
@@ -10,9 +10,15 @@ public class Task {
     }
 
 
-    static public int sortaSum(int a, int b) {
-        if (a+b>=10 && a+b<=19) return 20;
-        return a+b;
+    static public String alarmClock(int day, boolean vacation) {
+        if (vacation){
+            if (day==0 || day==6)return "off";
+            return "10:00";
+        }else {
+            if (day==0 || day==6)return "10:00";
+            return "7:00";
+        }
+
 
 
 
