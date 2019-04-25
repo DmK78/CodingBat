786d59e3d943c75005aaf50358c44d4a5e7976d1
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 16:58:45 +0500

    evenlySpaced

--- src/Task.java
+++ src/Task.java
@@ -6,30 +6,42 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(blackjack(19, 22));
+        System.out.println(evenlySpaced(2, 2, 2));
     }
 
 
-    static public int blackjack(int a, int b) {
+    static public boolean evenlySpaced(int a, int b, int c) {
+        int max = 0;
+        int middle = 0;
+        int min = 0;
+        int array[] = new int[3];
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
 
-        boolean overA = false;
-        boolean overB = false;
-        if (a < 0 || a > 21) overA = true;
-        if (b < 0 || b > 21) overB = true;
-        if (!overA && !overB) {
-            if (a > b) {
-                return a;
-            } else return b;
+        max = array[0];
+        min = array[0];
+        array[0] = a;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
         }
-        if (overA && !overB) {
-            return b;
+        for (int i : array) {
+            if (i != max && i != min) {
+                middle = i;
+            }
         }
-        if (!overA && overB) {
-            return a;
+        if (a == b && b == c) return true;
+        //System.out.println(min+" "+middle+" "+max);
+        if ((middle - min) == (max - middle)) {
+            return true;
         }
 
-
-        return 0;
+        return false;
 
 
     }
