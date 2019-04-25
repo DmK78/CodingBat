73699dd30693a3171d39a8a2c113e9622545ebc0
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 21:28:09 +0500

    sortaSum

--- src/Task.java
+++ src/Task.java
@@ -6,22 +6,15 @@ public class Task {
     public static void main(String[] args) {
 
 
-        System.out.println(makeBricks(1, 2, 7));
+        System.out.println();
     }
 
 
-    static public int caughtSpeeding(int speed, boolean isBirthday) {
-        if (!isBirthday){
-            if (speed<=60) return 0;
-            if (speed>=60 && speed<=80) return 1;
-            if (speed>80) return 2;
-        }else {
-            if (speed<=65) return 0;
-            if (speed>=65 && speed<=85) return 1;
-            if (speed>85) return 2;
-        }
+    static public int sortaSum(int a, int b) {
+        if (a+b>=10 && a+b<=19) return 20;
+        return a+b;
 
-return 0;
+
 
 
     }
