9c31beff40d75ffd11ed92d1244fad73dfe82849
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 21:25:32 +0500

    caughtSpeeding

--- src/Task.java
+++ src/Task.java
@@ -10,12 +10,19 @@ public class Task {
     }
 
 
-    static public boolean squirrelPlay(int temp, boolean isSummer) {
-        if (isSummer) {
-            if (temp >= 60 && temp <= 100) return true;
-        } else
-            if (temp >= 60 && temp <= 90) return true;
-        return false;
+    static public int caughtSpeeding(int speed, boolean isBirthday) {
+        if (!isBirthday){
+            if (speed<=60) return 0;
+            if (speed>=60 && speed<=80) return 1;
+            if (speed>80) return 2;
+        }else {
+            if (speed<=65) return 0;
+            if (speed>=65 && speed<=85) return 1;
+            if (speed>85) return 2;
+        }
+
+return 0;
+
 
     }
 
