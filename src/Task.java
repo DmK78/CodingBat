e65590f5df20dc4ca9dae78f70319dcabe1d85a4
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-25 20:56:31 +0500

    cigarParty

--- src/Task.java
+++ src/Task.java
@@ -10,18 +10,13 @@ public class Task {
     }
 
 
-    static public boolean makeBricks(int small, int big, int goal) {
-        int smallTotal = 0;
-        if (small + big * 5 < goal) {
-            return false;
-        }
-        if (goal / 5 - big > 0) {
-            smallTotal = (goal / 5 - big) * 5 + goal % 5;
+    static public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            if (cigars >= 40) return true;
         } else {
-            smallTotal = goal % 5;
+            if (cigars >= 40 && cigars <= 60) return true;
         }
-        if (smallTotal > small) return false;
-        return true;
+        return false;
     }
 
 }
