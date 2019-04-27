180354cfbef9208d03b3a4066333f53f54ee5c29
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-27 15:02:49 +0500

    love6

--- src/Task.java
+++ src/Task.java
@@ -10,18 +10,11 @@ public class Task {
     }
 
 
-    static public String alarmClock(int day, boolean vacation) {
-        if (vacation){
-            if (day==0 || day==6)return "off";
-            return "10:00";
-        }else {
-            if (day==0 || day==6)return "10:00";
-            return "7:00";
+    static public boolean love6(int a, int b) {
+        if (a == 6 || b == 6 || a + b == 6 || Math.abs(a - b) == 6) {
+            return true;
         }
-
-
-
-
+        return false;
 
     }
 
