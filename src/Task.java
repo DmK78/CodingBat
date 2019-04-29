95b693fdbf2eeff4670169bd9a2773bd401be2a4
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-29 10:06:06 +0500

    lastDigit

--- src/Task.java
+++ src/Task.java
@@ -10,14 +10,18 @@ public class Task {
     }
 
 
-    static public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
-        if (equalOk) {
-            if (a <= b && b <= c) {
-                return true;
-            }
-        } else if (a < b && b < c) {
+    static public boolean lastDigit(int a, int b, int c) {
+        if (rightMost(a) == rightMost(b) || rightMost(a) == rightMost(c) || rightMost(b) == rightMost(c)) {
             return true;
         }
         return false;
+
+    }
+
+    static int rightMost(int n) {
+        if (n >= 10) {
+            return n % 10;
+        }
+        return n;
     }
 }
