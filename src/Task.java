5d231fbea1f523c1adb99efa87b1c6bee81e8008
Dmitry Kolganov <dmk78@inbox.ru>
2019-04-30 20:59:58 +0500

    repeatSeparator

--- src/Task.java
+++ src/Task.java
@@ -9,13 +9,13 @@ public class Task {
         System.out.println(repeatSeparator("Word", "X", 3));
     }
 
-
+    //recursive
     static public String repeatSeparator(String word, String sep, int count) {
-        if (count==0) return "";
+        if (count == 0) return "";
         if (count == 1) {
-            return word;}
-            return word + sep + repeatSeparator(word, sep, count - 1);
-
+            return word;
+        }
+        return word + sep + repeatSeparator(word, sep, count - 1);
 
 
     }
